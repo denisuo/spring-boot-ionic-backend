3b017cf20ab90e9ac9e8f12c17043570568f9b16
Denis Oliveira <denis_uo@hotmail.com>
2019-01-14 23:11:18 -0300

    Personalizando o retorno da validacao

--- src/main/java/com/denisaraujo/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/denisaraujo/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -23,6 +25,20 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(DataIntegrityException.class)
 	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
 		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> dataIntegrity(MethodArgumentNotValidException e, HttpServletRequest request){
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+		
+		//TODO: método antigo FOREACH
+		//for(FieldError x : e.getBindingResult().getFieldErrors()) {
+		//	err.addError(x.getField(), x.getDefaultMessage());
+		//}
+		//método novo java 8
+		e.getBindingResult().getFieldErrors().forEach(fieldError -> err.addError(fieldError.getField(), fieldError.getDefaultMessage()));
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
