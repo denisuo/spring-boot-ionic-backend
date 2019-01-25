c563d9ee595494f746793ed55c3a262a3eacaa50
Denis Oliveira <denis_uo@hotmail.com>
2019-01-24 21:01:06 -0300

    Criando profile de teste

---
+++ src/main/java/com/denisaraujo/cursomc/config/TestConfig.java
@@ -0,0 +1,24 @@
+package com.denisaraujo.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.denisaraujo.cursomc.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instatiateDataBase() throws ParseException {
+		dbService.instatiateTestDataBase();
+		return true;
+	}
+}
