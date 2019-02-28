36d5178dae4eedc300cb1b2dd5b75be606840686
Denis Oliveira <denis_uo@hotmail.com>
2019-02-28 19:21:09 -0300

    Implementando a autorizacao

--- src/main/java/com/denisaraujo/cursomc/config/SecurityConfig.java
+++ src/main/java/com/denisaraujo/cursomc/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.denisaraujo.cursomc.security.JWTAuthorizationFilter;
 import com.denisaraujo.cursomc.security.JWTAuthenticationFilter;
 import com.denisaraujo.cursomc.security.JWTUtil;
 
@@ -57,7 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-		
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
