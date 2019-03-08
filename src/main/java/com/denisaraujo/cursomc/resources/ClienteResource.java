54fcf1de7369fd7471e129de32daae5ca2e77608
Denis Oliveira <denis_uo@hotmail.com>
2019-03-07 22:39:00 -0300

    Enviando imagem via endpoint

--- src/main/java/com/denisaraujo/cursomc/resources/ClienteResource.java
+++ src/main/java/com/denisaraujo/cursomc/resources/ClienteResource.java
@@ -16,11 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.denisaraujo.cursomc.domain.Categoria;
 import com.denisaraujo.cursomc.domain.Cliente;
-import com.denisaraujo.cursomc.dto.CategoriaDTO;
 import com.denisaraujo.cursomc.dto.ClienteDTO;
 import com.denisaraujo.cursomc.dto.ClienteNewDTO;
 import com.denisaraujo.cursomc.services.ClienteService;
@@ -89,5 +88,10 @@ public class ClienteResource {
 	}
 
 	
-	
+	@RequestMapping(value="/picture",method=RequestMethod.POST)
+	public ResponseEntity<Void> uploadProfilePicture(@RequestParam(name="file") MultipartFile file){
+		URI uri = service.uploadProfilePicture(file);
+		
+		return ResponseEntity.created(uri).build();
+	}
 }
