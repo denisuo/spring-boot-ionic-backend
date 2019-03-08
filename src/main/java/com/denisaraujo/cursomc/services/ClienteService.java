cb6c077d902f31c0e36e272e257ace9927799a0e
Denis Oliveira <denis_uo@hotmail.com>
2019-03-08 00:09:01 -0300

    Bonus: ajustando tamanho da imagem

--- src/main/java/com/denisaraujo/cursomc/services/ClienteService.java
+++ src/main/java/com/denisaraujo/cursomc/services/ClienteService.java
@@ -51,6 +51,9 @@ public class ClienteService {
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
 	
+	@Value("${img.profile.size}")
+	private Integer size;
+	
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
 		if(user == null || !user.hasHole(Perfil.ADMIN) && !id.equals(user.getId())) {
@@ -121,8 +124,6 @@ public class ClienteService {
 		return cli;
 	}
 	
-	
-	
 	private void updateData (Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
@@ -134,7 +135,10 @@ public class ClienteService {
 			throw new AuthorizationException("Acesso negado");
 		}
 		
-		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile); 
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
